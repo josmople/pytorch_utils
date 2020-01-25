99bee81bd6b7586a06126f905df8ef8d3f726353
Josm Ople <josmople@gmail.com>
2020-01-25 22:01:48 +0800

    Bugs and defaults

--- defaults.py
+++ defaults.py
@@ -2,3 +2,11 @@
 def image_loader(path):
     from PIL.Image import open
     return open(path)
+
+
+def vgg_normalize():
+    from torchvision.transforms import Normalize
+    return Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
