477f7c07eeb9ec8b79c7c748d3c36ca871646426
Josm Ople <josmople@gmail.com>
2020-01-27 23:36:46 +0800

    some bugs

--- check.py
+++ check.py
@@ -4,3 +4,11 @@ def is_int_or_pair(i):
     assert isinstance(i, (tuple, list))
     assert len(i) == 2
     return i
+
+
+def is_number_or_pair(i):
+    if isinstance(i, (int, float)):
+        i = i, i
+    assert isinstance(i, (tuple, list))
+    assert len(i) == 2
+    return i
