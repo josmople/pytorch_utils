d83263d2a57420f28c39360f74ea6e5484ffa9f9
Josm Ople <josmople@gmail.com>
2020-08-03 20:25:40 +0800

    rollback and optimization

--- features/image.py
+++ features/image.py
@@ -4,21 +4,18 @@ def gram(x):
     assert isinstance(x, Tensor)
     assert x.dim() == 4
 
-    N, C, H, W = x.size()
-    x = x.view(N, C, H * W)
+    B, C, H, W = x.size()
+    x = x.view(B, C, H * W)
     return bmm(x, x.permute(0, 2, 1)) / (H * W)
 
 
-def covariance(x, standardize=False, standardize_eps=1e-8):
+def covariance(x, eps=1e-8):
     from torch import Tensor, bmm
     assert isinstance(x, Tensor)
     assert x.dim() == 4
 
     B, C, H, W = x.size()
     x = x.view(B, C, H * W)
-    if standardize:
-        x_mean = x.mean(dim=2, keepdim=True)
-        x_std = x.std(dim=2, keepdim=True)
-        x = (x - x_mean) / (x_std + standardize_eps)
-        del x_mean, x_std
+    x.sub_(x.mean(dim=2, keepdim=True))
+    x.div_(x.std(dim=2, keepdim=True) + eps)
     return bmm(x, x.permute(0, 2, 1)) / (H * W)
