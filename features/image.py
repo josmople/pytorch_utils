a099b441ff97eff43e3fbcc7ec1b097275e2794b
Josm Ople <josmople@gmail.com>
2020-08-03 20:17:57 +0800

    added optional stadardization in covariance

--- features/image.py
+++ features/image.py
@@ -9,15 +9,16 @@ def gram(x):
     return bmm(x, x.permute(0, 2, 1)) / (H * W)
 
 
-def covariance(x, eps=1e-8):
+def covariance(x, standardize=False, standardize_eps=1e-8):
     from torch import Tensor, bmm
     assert isinstance(x, Tensor)
     assert x.dim() == 4
 
     B, C, H, W = x.size()
-    x_hat = x.view(B, C, H * W)
-    x_mean = x_hat.mean(dim=2, keepdim=True)
-    x_std = x_hat.std(dim=2, keepdim=True)
-    x_norm = (x_hat - x_mean) / (x_std + eps)
-    del x_hat, x_mean, x_std
-    return bmm(x_norm, x_norm.permute(0, 2, 1)) / (H * W)
+    x = x.view(B, C, H * W)
+    if standardize:
+        x_mean = x.mean(dim=2, keepdim=True)
+        x_std = x.std(dim=2, keepdim=True)
+        x = (x - x_mean) / (x_std + standardize_eps)
+        del x_mean, x_std
+    return bmm(x, x.permute(0, 2, 1)) / (H * W)
