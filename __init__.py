29f289aa061abaeef3e7acfa786d75adcf1fc56c
Josm Ople <josmople@gmail.com>
2020-08-18 18:50:20 +0800

    added transforms to root

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from . import _internal
 if _internal.load_lazy_submodules():
-    from . import ctx, data, events, features, log, metrics, nn, noise, dirp, file, lazyloader, runq, search, strf, vgg
+    from . import ctx, data, events, features, log, metrics, nn, noise, transforms, dirp, file, lazyloader, runq, search, strf, vgg
 
 from .functional import *
 
