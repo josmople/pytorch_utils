6e25ad111ad5bc5626cdee51388141b3c0701c70
Josm Ople <josmople@gmail.com>
2021-03-10 23:56:39 +0800

    some vgg bug

--- vgg.py
+++ vgg.py
@@ -154,13 +154,14 @@ class VGGExtractor(_Module):
         assert isinstance(inplace_relu, bool)
         assert callable(normalize_fn) or normalize_fn is None
 
-        model_name = f"vgg{vgg_nlayer}{'_bn' if vgg_bn else ''}"
-        self.model = getattr(models, model_name)(pretrained=pretrained, **kwds).features
+        vgg_name = f"vgg{vgg_nlayer}{'_bn' if vgg_bn else ''}"
+        vgg_features: _Sequential = getattr(models, vgg_name)(pretrained=pretrained, **kwds).features
+        initial_mapping = self.layername_index_mapping(vgg_features)
+        max_idx = (initial_mapping[str(max_layer)] + 1) if isinstance(max_layer, str) else max_layer
+
+        self.model = vgg_features[:max_idx]
         self.mapping = self.layername_index_mapping(self.model)
         self.normalize = normalize_fn
-
-        max_idx = (self.mapping[str(max_layer)] + 1) if isinstance(max_layer, str) else max_layer
-        self.model: _Sequential = self.model[:max_idx]
 
         for param in self.model.parameters():
             param.requires_grad_(requires_grad)
