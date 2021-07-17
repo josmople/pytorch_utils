dd5b395a685bb08ca8a873155bab6e03c5697e00
Josm Ople <josmople@gmail.com>
2021-07-18 03:02:59 +0800

    added more functionalities for vgg feature extractor

--- feat/vgg.py
+++ feat/vgg.py
@@ -1,5 +1,5 @@
 import typing as _T
-import torch.nn as _nn
+import torch as _th
 from .extractor import FeatureExtractor as _FeatureExtractor
 
 import torchvision.models.vgg as base
@@ -27,13 +27,11 @@ def normalize(tensor, mean=NORMALIZE_MEAN, std=NORMALIZE_STD, inplace=False):
     std = as_tensor(std, dtype=dtype, device=tensor.device)
 
     if tensor.dim() == 4:
-        tensor.sub_(mean[None, :, None, None]).div_(std[None, :, None, None])
-    elif tensor.dim() == 3:
-        tensor.sub_(mean[:, None, None]).div_(std[:, None, None])
-    else:
-        raise NotImplementedError("Only accepts 3D or 4D tensor")
+        return tensor.sub_(mean[None, :, None, None]).div_(std[None, :, None, None])
+    if tensor.dim() == 3:
+        return tensor.sub_(mean[:, None, None]).div_(std[:, None, None])
 
-    return tensor
+    raise NotImplementedError("Only accepts 3D or 4D tensor")
 
 
 def denormalize(tensor, mean=NORMALIZE_MEAN, std=NORMALIZE_STD, inplace=False):
@@ -52,13 +50,11 @@ def denormalize(tensor, mean=NORMALIZE_MEAN, std=NORMALIZE_STD, inplace=False):
     std = as_tensor(std, dtype=dtype, device=tensor.device)
 
     if tensor.dim() == 4:
-        tensor.mul_(std[None, :, None, None]).add_(mean[None, :, None, None])
-    elif tensor.dim() == 3:
+        return tensor.mul_(std[None, :, None, None]).add_(mean[None, :, None, None])
+    if tensor.dim() == 3:
         tensor.mul_(std[:, None, None]).add_(mean[:, None, None])
-    else:
-        raise NotImplementedError("Only accepts 3D or 4D tensor")
 
-    return tensor
+    raise NotImplementedError("Only accepts 3D or 4D tensor")
 
 
 def normalize_255(tensor, mean=NORMALIZE_MEAN_255, std=NORMALIZE_STD_255, inplace=False):
@@ -79,7 +75,7 @@ def denormalize_255(tensor, mean=NORMALIZE_MEAN_255, std=NORMALIZE_STD_255, inpl
     return denormalize(tensor, mean, std, inplace)
 
 
-def layernames(features: _T.List[_nn.Module]) -> _T.List[str]:
+def layernames(features: _T.List[_th.nn.Module]) -> _T.List[str]:
     """
     Generates a mapping of {layername: index} based on the VGG layers (i.e. features)
     """
@@ -104,28 +100,84 @@ def layernames(features: _T.List[_nn.Module]) -> _T.List[str]:
     return names
 
 
+class _Lambda(_th.nn.Module):
+
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, *args, **kwds):
+        return self.fn(*args, **kwds)
+
+
 class VggExtractor(_FeatureExtractor):
     """
     Allows extraction of features from specific layers of the VGG network.
     """
 
-    class Features(_nn.Sequential):
+    def __init__(
+        self,
+        targets: _T.List[str],
+        vgg_nlayers: int = 13,
+        vgg_bn: bool = False,
+        inplace_relu: bool = False,
+        freeze: bool = False,
+        pool: _T.Literal["max", "avg"] = "max",
+        preprocess: _T.Callable[[_th.Tensor], _th.Tensor] = normalize,
+        final_layer: _T.Union[str, int] = None,
+        pretrained: bool = True,
+        **kwds
+    ):
+        # Look for the specific vgg model
+        vgg_name = f"vgg{vgg_nlayers}{'_bn' if vgg_bn else ''}"
+        vgg_constructor = getattr(base, vgg_name)
 
-        def __init__(self, nlayers: int = 13, bn: bool = False, **kwds):
-            model_name = f"vgg{nlayers}{'_bn' if bn else ''}"
-            layers = getattr(base, model_name)(**kwds).features
-            names = layernames(layers)
+        # Extract the feature layers and their corresponding names
+        layers = vgg_constructor(pretrained=pretrained, **kwds).features
+        names = layernames(layers)
 
-            from collections import OrderedDict
-            modules = OrderedDict()
-            for name, layer in zip(names, layers):
-                modules[name] = layer
+        # Initialize the submodule storage
+        from collections import OrderedDict
+        modules = OrderedDict()
 
-            super().__init__(modules)
+        # Add the preprocessing layer if provided
+        if callable(preprocess):
+            preprocess = _Lambda(preprocess)
+            modules["preprocess"] = preprocess
 
-    def __init__(self, targets: _T.List[str], vgg_nlayers: int = 13, vgg_bn: bool = False, pretrained: bool = True, **kwds):
-        model = VggExtractor.Features(nlayers=vgg_nlayers, bn=vgg_bn, pretrained=pretrained, **kwds)
+        # Perform module-wise processing based on parameters
+        for idx, (name, layer) in enumerate(zip(names, layers)):
+
+            # In-place ReLU
+            if isinstance(layer, _th.nn.ReLU):
+                layer.inplace = inplace_relu
+
+            # Use average pool instead of max pool (useful for style transfer)
+            elif isinstance(layer, _th.nn.MaxPool2d):
+                if pool == "avg":
+                    alt_layer = _th.nn.AvgPool2d(
+                        kernel_size=layer.kernel_size,
+                        stride=layer.stride,
+                        padding=layer.padding
+                    )
+                    layer = alt_layer
+
+            modules[name] = layer
+
+            # Discard other layers if final_layer is reached
+            if name == final_layer or idx == final_layer:
+                break
+
+        # Agglomerate the modules
+        model = _th.nn.Sequential(modules)
+
+        # Freeze the model if desired
+        if freeze:
+            for p in model.parameters():
+                p.requires_grad = False
+
+        # Initialize feature extractor
         super().__init__(model, targets)
 
 
-del _T, _nn, _FeatureExtractor
+del _T, _th, _FeatureExtractor
