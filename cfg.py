f57032b17b2473cc5d0663a6be98e1aed5bfd078
Josm Ople <josmople@gmail.com>
2021-08-02 15:45:58 +0800

    refactor

--- cfg.py
+++ cfg.py
@@ -2,39 +2,38 @@ import typing as _T
 import argparse
 
 
-class ParseArgsDescriptor:
+class utils:
 
     @staticmethod
-    def get_namespace(obj: object, objtype: type) -> argparse.Namespace:
-        src = obj or objtype
-
+    def get_namespace(src: object, namespace_attr="__namespace__") -> argparse.Namespace:
         keys1 = getattr(src, "__dict__", {}).keys()
         keys2 = getattr(src, "__slots__", [])
 
         for name in [*keys1, *keys2]:
-            if name == "__namespace__":
-                return getattr(src, "__namespace__")
+            if name == namespace_attr:
+                return getattr(src, namespace_attr)
+
         return None
 
     @staticmethod
-    def set_namespace(obj: object, objtype: type, val: argparse.Namespace):
-        src = obj or objtype
-        setattr(src, "__namespace__", val)
+    def set_namespace(src: object, val: argparse.Namespace, namespace_attr="__namespace__"):
+        setattr(src, namespace_attr, val)
 
     @staticmethod
-    def generate_parser(obj: object, objtype: type) -> argparse.ArgumentParser:
-        from inspect import getattr_static
+    def create_parser_instance(config: object) -> argparse.ArgumentParser:
+        if isinstance(config, type):
+            if issubclass(config, argparse.ArgumentParser):
+                return config()
 
-        if isinstance(obj, argparse.ArgumentParser):
+        if isinstance(config, argparse.ArgumentParser):
             from copy import deepcopy
-            parser = deepcopy(obj)
-        elif issubclass(objtype, argparse.ArgumentParser):
-            parser = objtype()
-        else:
-            parser = argparse.ArgumentParser()
+            return deepcopy(config)
 
-        # Apply parser arguments
-        config = obj or objtype
+        return argparse.ArgumentParser()
+
+    @staticmethod
+    def apply_parser_config(parser: argparse.ArgumentParser, config: object) -> argparse.ArgumentParser:
+        from inspect import getattr_static
 
         for key in dir(config):
             val = getattr_static(config, key)
@@ -46,19 +45,42 @@ class ParseArgsDescriptor:
         return parser
 
     @staticmethod
-    def generate_namespace(obj: object, objtype: type, parser: argparse.ArgumentParser):
-        src = obj or objtype
-        custom_parameters = getattr(src, "__parameters__", None)
+    def parse_args(parser: argparse.ArgumentParser, config: object, parameter_attr="__parameters__"):
+        custom_parameters = getattr(config, parameter_attr, None)
         if custom_parameters is None:
             return parser.parse_args()
         return parser.parse_args(custom_parameters)
+
+
+class ParseArgsDescriptor:
+
+    @staticmethod
+    def get_namespace(obj: object, objtype: type) -> argparse.Namespace:
+        src = obj or objtype
+        return utils.get_namespace(src)
+
+    @staticmethod
+    def set_namespace(obj: object, objtype: type, val: argparse.Namespace):
+        src = obj or objtype
+        utils.set_namespace(src, val)
+
+    @staticmethod
+    def create_parser(obj: object, objtype: type) -> argparse.ArgumentParser:
+        config = obj or objtype
+        parser = utils.create_parser_instance(config)
+        return utils.apply_parser_config(parser, config)
+
+    @staticmethod
+    def create_namespace(obj: object, objtype: type, parser: argparse.ArgumentParser):
+        config = obj or objtype
+        return utils.parse_args(parser, config)
 
     def namespace(self, obj: object, objtype: type):
         namespace = self.get_namespace(obj, objtype)
 
         if namespace is None:
-            parser = self.generate_parser(obj, objtype)
-            raw_namespace = self.generate_namespace(obj, objtype, parser)
+            parser = self.create_parser(obj, objtype)
+            raw_namespace = self.create_namespace(obj, objtype, parser)
 
             self.set_namespace(obj, objtype, raw_namespace)
             namespace = self.get_namespace(obj, objtype)
@@ -76,9 +98,6 @@ class ParseArgsDescriptor:
     def __get__(self, obj: object, objtype: type = None):
         namespace = self.namespace(obj, objtype)
         return self.attribute(namespace)
-
-    def __set__(self, obj, value):
-        print(obj, value)
 
     def __str__(self):
         args = ", ".join([f"{v!r}" for v in self.args])
