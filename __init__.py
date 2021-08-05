219897844a681ba75970682f0154074f307cef07
Josm Ople <josmople@gmail.com>
2021-08-05 17:01:10 +0800

    fixed init

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from . import _internal
 if _internal.lazyload_submodules():
-    from . import cfg, data, decorators, dirp, feat, paths, tb
+    from . import az, cfg, data, decorators, dirp, iz, feat, paths, tb
 
 from .dirp import dirpath, ts
