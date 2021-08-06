0c5a1468a4f4385297542e290904380c9525783f
Josm Ople <josmople@gmail.com>
2021-08-06 17:34:16 +0800

    docs

--- resize.py
+++ resize.py
@@ -10,13 +10,13 @@ class Resize:
         self.scale: _T.Tuple[int, int] = scale
         self.kwds = kwds
 
-    def chw(self, tensor: _th.Tensor, *, mode=None, size=None, scale=None, **kwds):
+    def chw(self, tensor: _th.Tensor, *, mode=None, size=None, scale=None, **kwds) -> _th.Tensor:
         assert tensor.dim() == 3
         tensor = tensor.unsqueeze(0)
         tensor = self.bchw(tensor, mode=mode, size=size, scale=scale, **kwds)
         return tensor.squeeze(0)
 
-    def bchw(self, tensor: _th.Tensor, *, mode=None, size=None, scale=None, **kwds):
+    def bchw(self, tensor: _th.Tensor, *, mode=None, size=None, scale=None, **kwds) -> _th.Tensor:
         assert tensor.dim() == 4
         from torch.nn.functional import interpolate
         local_kwds = self.kwds.copy()
