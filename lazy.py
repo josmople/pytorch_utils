d66501763cc120389b075f54028c75688e8c5194
Josm Ople <josmople@gmail.com>
2021-08-07 01:30:48 +0800

    improved lazy

--- lazy.py
+++ lazy.py
@@ -12,53 +12,64 @@ class LazyLoader(_ModuleType):
     """
 
     # The lint error here is incorrect.
-    def __init__(self, local_name, parent_module_globals, name):  # pylint: disable=super-on-old-class
-        self._local_name = local_name
+    def __init__(self, name, parent_module_globals):  # pylint: disable=super-on-old-class
         self._parent_module_globals = parent_module_globals
+        self._module_cache = None
 
         super(LazyLoader, self).__init__(name)
 
-    def _load(self):
+    @property
+    def _module(self):
+        if self._module_cache is not None:
+            return self._module_cache
+
         from importlib import import_module
 
         # Import the target module and insert it into the parent's namespace
         module = import_module(self.__name__)
-        self._parent_module_globals[self._local_name] = module
+
+        # Find the name of this object (LazyLoader) in the parent's namespace
+        # Then insert the actual module in place of this object (LazyLoader)
+        for local_name, value in self._parent_module_globals:
+            if value == self:
+                self._parent_module_globals[local_name] = module  # Replace
+                break
 
         # Update this object's dict so that if someone keeps a reference to the LazyLoader,
         # lookups are efficient (__getattr__ is only called on lookups that fail).
         self.__dict__.update(module.__dict__)
 
+        self._module_cache = module
         return module
 
+    def __call__(self, *args, **kwds):
+        return self._module(*args, **kwds)
+
     def __getattr__(self, item):
-        module = self._load()
-        return getattr(module, item)
+        return getattr(self._module, item)
 
     def __dir__(self):
-        module = self._load()
-        return dir(module)
+        return dir(self._module)
 
 
-def lazyload(localname, name=None, module_globals=None):
-    if name is None:
-        name = localname
+def lazyload(name, module_globals=None):
     if module_globals is None:
         from sys import _getframe
         module_globals = _getframe(1).f_globals
-
-    ll = LazyLoader(localname, module_globals, name)
-    module_globals[localname] = ll
-    return False
+    return LazyLoader(name, module_globals)
 
 
-def lazyload_submodules(root_modname=None, include_submodules=None, exclude_submodules=None, stack_idx=1) -> True:
+def lazyload_submodules(root_module_name=None, include_submodules=None, exclude_submodules=None, stack_idx=1) -> True:
     """
-    Searches all possible submodules and makes them lazily-loaded
-    root_modname - defaults to the dirname
-    include_submodules - if not none, will reject not in the list
-    exclude_submodules - if not none, will reject in the list
-    stack_idx - frame references stack (1 -> the scope of the package/code that called this function)
+    Searches all possible submodules and makes them lazily-loaded.
+
+    root_module_name - defaults to the __package__.
+
+    include_submodules - if not none, will reject not in the list.
+
+    exclude_submodules - if not none, will reject in the list.
+
+    stack_idx - frame references stack (1 -> the scope of the package/code that called this function).
     """
 
     from sys import _getframe
@@ -67,28 +78,29 @@ def lazyload_submodules(root_modname=None, include_submodules=None, exclude_subm
 
     variables = _getframe(stack_idx).f_globals
     filepath = variables["__file__"]
-    root_modname = root_modname or variables["__package__"]
+    root_module_name = root_module_name or variables["__package__"]
 
     # Load utils/{submodule}.py
-    modpaths = glob(f"{dirname(filepath)}/**.py")
-    modpaths = filter(lambda p: not p.endswith("__init__.py"), modpaths)
-    modnames = map(lambda p: splitext(basename(p))[0], modpaths)
-    if include_submodules is not None:
-        modnames = filter(lambda n: n in include_submodules, modnames)
-    if exclude_submodules is not None:
-        modnames = filter(lambda n: n not in exclude_submodules, modnames)
-    for modname in modnames:
-        variables[modname] = LazyLoader(modname, variables, f"{root_modname}.{modname}")
+    file_submodules = glob(f"{dirname(filepath)}/**.py")
+    file_submodules = filter(lambda p: not p.endswith("__init__.py"), file_submodules)
+    file_submodules = map(lambda p: splitext(basename(p))[0], file_submodules)
 
     # Load utils/{submodule}/__init__.py
-    modpaths = glob(f"{dirname(filepath)}/**/__init__.py")
-    modnames = map(lambda p: basename(dirname(p)), modpaths)
+    dir_submodules = glob(f"{dirname(filepath)}/**/__init__.py")
+    dir_submodules = map(lambda p: basename(dirname(p)), dir_submodules)
+
+    # All submodule names
+    submodules = list(file_submodules) + list(dir_submodules)
+
+    # Perform filter
     if include_submodules is not None:
-        modnames = filter(lambda n: n in include_submodules, modnames)
+        submodules = filter(lambda n: n in include_submodules, submodules)
     if exclude_submodules is not None:
-        modnames = filter(lambda n: n not in exclude_submodules, modnames)
-    for modname in modnames:
-        variables[modname] = LazyLoader(modname, variables, f"{root_modname}.{modname}")
+        submodules = filter(lambda n: n not in exclude_submodules, submodules)
+
+    # Assign to parent namespace
+    for submodule_name in submodules:
+        variables[submodule_name] = LazyLoader(f"{root_module_name}.{submodule_name}", variables)
 
     # Put the function call in an if statement for intellisense
     # See example below
