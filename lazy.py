b0d00c2c1c03e9a7bc25c6d1d238fb8e00105276
Josm Ople <josmople@gmail.com>
2021-08-06 15:18:44 +0800

    removed some stuff

--- lazy.py
+++ lazy.py
@@ -1,10 +1,6 @@
 # Code copied from https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/util/lazy_loader.py
 """A LazyLoader class."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 
 from types import ModuleType as _ModuleType
 
