482dba48dd96b9d79d9bf0dc14ba1020db28f31c
Josm Ople <josmople@gmail.com>
2022-01-31 14:13:57 +0800

    better collate

--- collate.py
+++ collate.py
@@ -7,28 +7,34 @@ def collate_items(col):
 
 
 def generate_collate_batch(concat_dict: T.Dict[T.Tuple[int, type], T.Callable[[T.List], T.Any]] = dict(), concat_default: T.Callable[[T.List], T.Any] = collate_items):
+    concat_dict = concat_dict.copy()
 
-    def collate_batch(batch):
-        db: T.Dict[int, T.List] = {}
+    def collate_col(items: list, *keys) -> T.Any:
+        for key in keys:
+            if key in concat_dict:
+                return concat_dict[key](items)
+        return concat_default(items)
 
-        for row_idx, row in enumerate(batch):
-            for col_idx, cell in enumerate(row):
-                if col_idx not in db:
-                    db[col_idx] = []
-                db[col_idx].append(cell)
+    def collate_batch(batch: T.Union[T.Mapping, T.NamedTuple, T.Sequence]) -> T.Any:
+        row = batch[0]
+        row_type = type(row)
 
-        cols = []
+        if isinstance(row, T.Mapping):
+            return {key: collate_col([d[key] for d in batch], key, type(row[key])) for key in row}
 
-        for col_idx, col in db.items():
-            if col_idx in concat_dict:
-                fn = concat_dict[col_idx]
-            elif type(col[0]) in concat_dict:
-                fn = concat_dict[type(col[0])]
-            else:
-                fn = concat_default
-            col = fn(col)
-            cols.append(col)
+        if isinstance(row, tuple) and hasattr(row, '_fields'):  # namedtuple
+            transposed = zip(*batch)
+            return row_type(*(collate_col(samples, idx, key, type(samples[0])) for samples, idx, key in zip(transposed, range(len(row), row._fields))))
 
-        return cols
+        if isinstance(row, T.Sequence):
+            # check to make sure that the elements in batch have consistent size
+            it = iter(batch)
+            elem_size = len(next(it))
+            if not all(len(elem) == elem_size for elem in it):
+                raise RuntimeError('each element in list of batch should be of equal size')
+            transposed = zip(*batch)
+            return [collate_col(samples, idx, type(samples[0])) for idx, samples in enumerate(transposed)]
+
+        raise TypeError(f"Collation method not found for type {row_type}")
 
     return collate_batch
