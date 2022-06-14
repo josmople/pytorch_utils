58ef81094db1a5ec9809498c1b320716baf29a78
josmople <josmople@gmail.com>
2022-06-14 10:02:36 +0800

    added save and load for state dict

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from .lazy import lazyload_submodules as _lazyload_submodules
 if _lazyload_submodules():
-    from . import az, cfg, collate, data, dirp, fn, im, lazy, log, iz, feat, paths, resize, strf, tags, tvision
+    from . import az, cfg, collate, data, dirp, fn, im, io, lazy, log, iz, feat, paths, resize, strf, tags, tvision
---
+++ io.py
@@ -0,0 +1,18 @@
+def state_dicts(obj: object):
+    state_dict = {}
+    for prop_name in dir(obj):
+        prop_value = getattr(obj, prop_name, None)
+        state_dict_provider = getattr(prop_value, "state_dict", None)
+        if callable(state_dict_provider):
+            prop_state = state_dict_provider()
+            state_dict[prop_name] = prop_state
+    return state_dict
+
+
+def load_state_dicts(obj: object, state_dict: dict):
+    for prop_name in state_dict:
+        prop_state = state_dict[prop_name]
+        prop_value = getattr(obj, prop_name, None)
+        state_dict_consumer = getattr(prop_value, "load_state_dict", None)
+        if callable(state_dict_consumer):
+            state_dict_consumer(prop_state)
